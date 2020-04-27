27f9df9bc17c272a83361c7ca09835b77d5b4026
Aliaksandr <cncntrt@gmail.com>
2020-04-27 16:04:48 +0300

    edited `copy_project_to_server.sh`

--- copy_project_to_server.sh
+++ copy_project_to_server.sh
@@ -1,2 +1,11 @@
-shopt -s extglob
-scp -P ${LUNGS_SERVER_PORT} -r !(venv|notebooks|results) ${LUNGS_SERVER_ADDRESS}:~/dev/lungs/
+# should be executed with `source` or `.` command:
+# `source copy_project_to_server.sh`
+
+# `--exclude` option excludes folders and SUBFOLDERS that have corresponding names
+# `--dry-run` option does not copy anything but simulates the process
+rsync -v -r \
+  -e "ssh -p ${LUNGS_SERVER_PORT}" \
+  --exclude={'venv','.git','.idea','notebooks','results','__pycache__','.gitignore'} \
+  . \
+  "${LUNGS_SERVER_ADDRESS}:~/dev/lungs/" \
+#  --dry-run
